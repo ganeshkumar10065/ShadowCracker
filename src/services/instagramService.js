8018cadae84ead8b3aa9ec1fc6493d4bee503bb9
ganeshkumar10065 <wergerdifeok.sv.r.r.1058@gmail.com>
2025-06-02 20:56:05 +0530

    Update instagramService.js

--- src/services/instagramService.js
+++ src/services/instagramService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.43.161:3001/api';
+const API_BASE_URL = 'http://13.49.21.2:3001/api';
 
 // List of session cookies (sessionid and csrftoken pairs)
 const accounts = [
